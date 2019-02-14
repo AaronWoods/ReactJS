5fae0310861b54b651a3b881a4421976aa7a3ed6
AaronWoods <aaron.woods22@mail.dcu.ie>
2019-02-14 12:10:18 +0000

    properties routing - verbose syntax

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -4,13 +4,91 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// properties routing - verbose syntax 
+class Display extends React.Component<{ color: string, num:string, size:string }>{
+
+    render(){
+        return (
+            <div>
+                {this.props.color}
+                {this.props.num}
+                {this.props.size}
+            </div>
+        );
+    }
+}
+
+// class Label extends React.Component<{ color: string, num:string, size:string }>{
+
+//     render(){
+//         return (
+//             <div>
+//                 <Display
+//                     color={this.props.color}
+//                     num={this.props.num}
+//                     size={this.props.size}
+//                 />
+//             </div>
+//         );
+//     }
+// }
+
+
+class Label extends React.Component<{ color: string, num:string, size:string }>{
+
+    render(){
+        return (
+            <div>
+                <Display
+                    {...this.props}
+                />
+            </div>
+        );
+    }
+}
+
+
+// class Shirt extends React.Component<{ color: string, num:string, size:string }>{
+
+//     render(){
+//         return (
+//             <div>
+                
+//                 <Label
+//                     color={this.props.color}
+//                     num={this.props.num}
+//                     size={this.props.size}
+//                 />
+                
+//             </div>
+//         );
+//     }
+// }
+
+class Shirt extends React.Component<{ color: string, num:string, size:string }>{
+
+    render(){
+        return (
+            <div>
+                
+                <Label
+                    {...this.props}
+                />
+                
+            </div>
+        );
+    }
+}
 
 
 
 
 ReactDOM.render(
-    <div>
-    </div>,
+    <Shirt
+        color="red"
+        num ="3.14"
+        size="big"
+    />,
 document.getElementById('container'));
 
 
