4d585be4e41da1076a7b132e263b2b90ccdd3812
AaronWoods <aaron.woods22@mail.dcu.ie>
2019-02-14 14:05:11 +0000

    Ticker lifecycle

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -4,31 +4,58 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-class Letter extends React.Component<{bgColour:string}>{
-    
-    render(){
 
-        let letterStyle= {
-            padding: 10,
-            margin: 10,
-            fontSize: 30,
-            backgroundColor: this.props.bgColour, //"#ffde00",
-            display: "inline-block"
-        }
-        return <div
-            style = {
-                letterStyle
-            }
-        >
-        {this.props.children}</div>
+// state is private, not shared
+//initialize state in constructor
+
+class LightingCounter extends React.Component<any,any>{
+    constructor(props:any){
+        super(props);
+
+        // we init state only in the constructor
+        //state readable and writable
+        this.state = { strikes:0 }
+
     }
+
+    timerTick(){
+        this.setState((prevState:any, props:any) => ( {
+                strikes: prevState.strikes +1
+            }
+        ))
+    }
+
+    //lifecycle methods
+    componentWillMount(){
+        console.log("in mounted");
+
+        //temporization
+        setInterval(() => this.timerTick(), 1000);
+    }
+
+    
+
+    
+
+    render(){
+        console.log("render");
+        return <h1>{this.state.strikes}</h1>
+    }
+    
+    
+}
+
+class LightingCounterParent extends React.Component{
+    render(){
+        return <LightingCounter/>
+    }
+    
+    
 }
 
 ReactDOM.render(
     <div>
-        <Letter bgColour="yellow">A</Letter>
-        <Letter bgColour="blue">B</Letter>
-        <Letter bgColour="red">C</Letter>
+        <LightingCounterParent/>
     </div>,
 document.getElementById('root'));
 
