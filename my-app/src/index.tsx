32dbca3c67f5d452307b0f78a29a2dd5dd1408f8
AaronWoods <aaron.woods22@mail.dcu.ie>
2019-02-14 13:42:56 +0000

    dynamic styling

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -4,11 +4,33 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class Letter extends React.Component<{bgColour:string}>{
+    
+    render(){
 
+        let letterStyle= {
+            padding: 10,
+            margin: 10,
+            fontSize: 30,
+            backgroundColor: this.props.bgColour, //"#ffde00",
+            display: "inline-block"
+        }
+        return <div
+            style = {
+                letterStyle
+            }
+        >
+        {this.props.children}</div>
+    }
+}
 
 ReactDOM.render(
-    <div/>,
-document.getElementById('container'));
+    <div>
+        <Letter bgColour="yellow">A</Letter>
+        <Letter bgColour="blue">B</Letter>
+        <Letter bgColour="red">C</Letter>
+    </div>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
