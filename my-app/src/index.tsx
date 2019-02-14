767178012299d6b8473f2f0bca7dece756f36c81
AaronWoods <aaron.woods22@mail.dcu.ie>
2019-02-14 15:05:17 +0000

    mounting phase

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -5,116 +5,61 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { func } from 'prop-types';
 
+/** 
+ * mounting phase
+ * updating phase
+ * unmounting phase
+*/
 
-// state is private, not shared
-// initialize state in constructor
-
-class LightingCounter extends React.Component<any,any>{
-    timerId:any;
-
-    constructor(props:any){
+class Logger extends React.Component<{time:string}> {
+    // mounting phase - constructor + componentWillMount + render + componentDidMount
+    constructor(props : {time:string}) {
         super(props);
 
-        // we init state only in the constructor
-        //state readable and writable
-        this.state = { strikes:0 }
-
+        console.log("in constructor");
     }
 
-    timerTick(){
-        this.setState((prevState:any, props:any) => ( {
-                strikes: prevState.strikes + props.increment
-            }
-        ))
-    }
-
-    //lifecycle methods
     componentWillMount(){
-        console.log("in mounted");
-
-        //temporization
-        this.timerId = setInterval(() => this.timerTick(), 1000);
+        console.log("will mount");
     }
-
-    componentWillUnmount(){
-        clearInterval(this.timerId);
-    }
-    
-
-    
-
-    render(){
-        console.log("render");
-        return <h1>{this.state.strikes}</h1>
-    }
-    
-    
-}
-
-class LightingCounterParent extends React.Component{
-    render(){
-        return <LightingCounter increment={5}/>
-    }
-    
-    
-}
-
-
-//pure components - we can have a degree of control on the re-render of component
-//function cant have a state 
-//no lifecycle hooks
-
-function MyPureComponent1(props: {username:string}){
-    console.log("in render");
-    return <div>{props.username}</div>
-}
-
-function areEqual(prevProps:{username:string}, newProps:{username:string}){
-    console.log("in areEqual");
-    return false; // flag controls re-render
-}
-
-// shallow properties comparison
-
-//class ParentComp extends React.Component<any,{username:string}>{ //not shallow - 
-class ParentComp extends React.PureComponent<any,{username:string}>{  //shallow - checks only incoming props for re-render - cant use shouldComponentUpdate lifecycle hook
-    mycomp:any;
-
-    constructor(props:{username:string}){
-        super(props)
-        this.state = {username: "user1"}
-        this.mycomp = React.memo(MyPureComponent1, areEqual); //component def, not instance
-    }
-    
-    // shouldComponentUpdate(){
-    //     return false;
-    // }
 
     componentDidMount(){
-        console.log("in componentDidMount");
-        this.setState((prevState, prevProps) => {
-            return  {username : "newval" }
-        })
-    };
+        //scheduling some logic
+        
+        console.log("did mount");
+
+        // we need a reference to native DOM fragment
+        console.log("DOM node:" + ReactDOM.findDOMNode(this));
+    }
+
+    render(){
+        console.log("in render");
+        return (
+            <div>
+                {this.props.time}
+            </div>
+        )
+    }
+}
+
+class LoggerParent extends React.Component<any,{currentTime:string}> {
+    constructor(props:any){
+        super(props);
+        this.state = {currentTime:new Date().toLocaleDateString() }
+    }
     
     render(){
         return (
-            <this.mycomp username={this.state.username}/>
+            <Logger time={this.state.currentTime} />
         )
     }
 }
 
 ReactDOM.render(
     <div>
-        <ParentComp />
+        <LoggerParent />
     </div>,
 document.getElementById('root'));
-
-// ReactDOM.render(
-//     <div>
-//         <LightingCounterParent/>
-//     </div>,
-// document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ my-app/src/tests/state/index.tsx
@@ -0,0 +1,122 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+// import { func } from 'prop-types';
+
+
+// // state is private, not shared
+// // initialize state in constructor
+
+// class LightingCounter extends React.Component<any,any>{
+//     timerId:any;
+
+//     constructor(props:any){
+//         super(props);
+
+//         // we init state only in the constructor
+//         //state readable and writable
+//         this.state = { strikes:0 }
+
+//     }
+
+//     timerTick(){
+//         this.setState((prevState:any, props:any) => ( {
+//                 strikes: prevState.strikes + props.increment
+//             }
+//         ))
+//     }
+
+//     //lifecycle methods
+//     componentWillMount(){
+//         console.log("in mounted");
+
+//         //temporization
+//         this.timerId = setInterval(() => this.timerTick(), 1000);
+//     }
+
+//     componentWillUnmount(){
+//         clearInterval(this.timerId);
+//     }
+    
+
+    
+
+//     render(){
+//         console.log("render");
+//         return <h1>{this.state.strikes}</h1>
+//     }
+    
+    
+// }
+
+// class LightingCounterParent extends React.Component{
+//     render(){
+//         return <LightingCounter increment={5}/>
+//     }
+    
+    
+// }
+
+
+// //pure components - we can have a degree of control on the re-render of component
+// //function cant have a state 
+// //no lifecycle hooks
+
+// function MyPureComponent1(props: {username:string}){
+//     console.log("in render");
+//     return <div>{props.username}</div>
+// }
+
+// function areEqual(prevProps:{username:string}, newProps:{username:string}){
+//     console.log("in areEqual");
+//     return false; // flag controls re-render
+// }
+
+// // shallow properties comparison
+
+// //class ParentComp extends React.Component<any,{username:string}>{ //not shallow - 
+// class ParentComp extends React.PureComponent<any,{username:string}>{  //shallow - checks only incoming props for re-render - cant use shouldComponentUpdate lifecycle hook
+//     mycomp:any;
+
+//     constructor(props:{username:string}){
+//         super(props)
+//         this.state = {username: "user1"}
+//         this.mycomp = React.memo(MyPureComponent1, areEqual); //component def, not instance
+//     }
+    
+//     // shouldComponentUpdate(){
+//     //     return false;
+//     // }
+
+//     componentDidMount(){
+//         console.log("in componentDidMount");
+//         this.setState((prevState, prevProps) => {
+//             return  {username : "newval" }
+//         })
+//     };
+    
+//     render(){
+//         return (
+//             <this.mycomp username={this.state.username}/>
+//         )
+//     }
+// }
+
+// ReactDOM.render(
+//     <div>
+//         <ParentComp />
+//     </div>,
+// document.getElementById('root'));
+
+// // ReactDOM.render(
+// //     <div>
+// //         <LightingCounterParent/>
+// //     </div>,
+// // document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
