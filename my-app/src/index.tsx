16f99682193b6e45ff7ba54e909cc70afe5ce5a8
AaronWoods <aaron.woods22@mail.dcu.ie>
2019-02-14 10:14:12 +0000

    interface

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -5,14 +5,30 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<h1>Startup</h1>, document.getElementById('container'));
+//ReactDOM.render(<h1>Startup</h1>, document.getElementById('container'));
 
 // Introduction to JSX
+interface User{
+    firstName: string,
+    lastName: string
+}
 
+function formatName(user:User) {
+    return user.firstName + ", " + user.lastName;
+}
 
+const user:User = {
+    firstName:"Aaron",
+    lastName:"Woods"
+}
 
+const element = (
+    <h1>
+        { formatName(user) }
+    </h1>
+)
 
-
+ReactDOM.render(element, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
