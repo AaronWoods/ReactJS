0adc54b23290bc83738f02cc546039943492e8a2
AaronWoods <aaron.woods22@mail.dcu.ie>
2019-02-14 15:55:44 +0000

    Input event handler - boil water calculator

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,106 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
-import { func } from 'prop-types';
 
-/** 
- * mounting phase
- * updating phase
- * unmounting phase
-*/
-
-class Logger extends React.Component<{time:string}> {
-    // mounting phase - constructor + componentWillMount + render + componentDidMount
-    constructor(props : {time:string}) {
-        super(props);
-
-        console.log("In Constructor");
+// lift the state up: where to declare the state 
+function PrintOutcome(props:{celcius:number}) {
+    if (props.celcius >100) {
+        return <p>The water will boil</p>
     }
 
-    componentWillMount(){
-        console.log("Will Mount");
-    }
-
-    componentDidMount(){
-        //scheduling some logic
-        
-        console.log("Did Mount");
-
-        // we need a reference to native DOM fragment
-        console.log("DOM node:" + ReactDOM.findDOMNode(this));
-    }
-
-    // updating phase: componentWillReceiveProps + shouldComponentUpdate + componentWillUpdate + render + componentDidUpdate 
-
-    componentWillReceiveProps(nextProps : {time:string}){
-        console.log("Will Receive: " + nextProps.time);
-    }
-
-    shouldComponentUpdate(nextProps: {time:string}, nextState:{}){
-        console.log("Should Update");
-        return true; // flat to render or not
-    }
-
-    componentWillUpdate(prevProps: {time:string}, prevState:{}){
-        console.log("Will Update");
-    }
-
-    componentDidUpdate(prevProps: {time:string}, prevState:{}, snapshot:any){
-        console.log("Did Update");
-    }
-
-    //rarely use method
-    // getSnapshotBeforeUpdate(prevProps: {time:string}, prevState:{}){
-    //     console.log("snapshot before update");
-
-    //     return 10; //meaningful to send to the componentDidUpdate method
-    // }
-
-    // unmounting phase
-    componentWillUnmount(){
-        console.log("Will Unmount");
-    }
-
-    
-    render(){
-        console.log("In render");
-        return (
-            <div>
-                {this.props.time}
-            </div>
-        )
-    }
+    return <p>The water would NOT boil</p>
 }
 
-class LoggerParent extends React.Component<any,{count:number, currentTime:string}> {
+class Calculator extends React.Component<any, { temperature:string }>{
+
     constructor(props:any){
         super(props);
-        this.state = {currentTime:new Date().toLocaleDateString(), count:0}
-        this.launchClock();
+        this.state = { temperature: "" };
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    launchClock(){
-        setInterval(() => {
-            this.setState({currentTime:new Date().toLocaleDateString(), count:this.state.count+1})
-        }, 1000);
+    handleChange(e:any){
+        this.setState( { temperature: e.target.value })
     }
-    
+
     render(){
-        if(this.state.count > 3){  
-            return <div />
-        }
+        const temperature = this.state.temperature;
         return (
-            
-            <Logger time={this.state.currentTime} />
+            <div>
+                Enter a temperature:
+                <input value={temperature} onChange={this.handleChange}/>
+                <PrintOutcome celcius={parseFloat(temperature)} />
+            </div>
+
         )
     }
+    
 }
 
 ReactDOM.render(
     <div>
-        <LoggerParent />
+        <Calculator />
     </div>,
 document.getElementById('root'));
 
